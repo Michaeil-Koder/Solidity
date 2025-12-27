3ab5ceef7611cb6d41ac5effc207dadc04cfa615
Michaeil-Koder <michaeil.zare1382@gmail.com>
2025-12-27 20:23:14 +0330

    constructor

--- contracts/contract.sol
+++ contracts/contract.sol
@@ -1,8 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.31;
 
+
+
 contract sample {
-    string Name;
+    enum GenderPerson {
+    Male,
+    Female
+    }
+    string private Name;
+    int private Age;
+    GenderPerson private Gender;
+
+    constructor(string memory name,int age,GenderPerson gender) {
+        Name = name;
+        Age = age;
+        Gender = gender;
+    }
+
     function setName(string memory name) public  {
         Name = name;
     }
